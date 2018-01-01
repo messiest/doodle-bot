cd95737482cd226560cfdb6579824d19291c8ef0
Chris Messier <messiercr@gmail.com>
2018-01-01 18:23:04 -0500

    removed redundant files

--- doodle-bot/data_handler.py
+++ doodle-bot/data_handler.py
@@ -104,7 +104,7 @@ def main():
     data = get_image_files(10)
     print(data)
     for i in run_session(data, 1):
-        print(i, "###" * 40)
+        print(i, "\n", "###" * 40)
 
 
 if __name__ == "__main__":
